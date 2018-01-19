e15a395a2b988fa2c6d3e8cc2ecfb1d514fadd9a
Dmitriy <ikeyten@gmail.com>
2018-01-19 19:16:32 +0300

    Update extramath.js

--- src/extramath.js
+++ src/extramath.js
@@ -1,1 +1,18 @@
+(function(provider, context){
+	if(typeof module === 'object' && typeof module.exports === 'object'){
+		module.exports = provider();
+	} else if(typeof define === 'function' && define.amd){
+		// todo: it will not work
+		define('extramath', [], function(){
+			return provider();
+		});
+	} else {
+		context.extramath = context.em = provider();
+	}
+})(function(){
+	var extramath = {
+		version: '0.0.1'
+	};
 
+	return extramath;
+}, this);
